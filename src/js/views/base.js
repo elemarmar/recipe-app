1762a229e2775b728ec04bd0922d4af8fa68df86
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-09-04 15:47:26 +0200

    Add base.js

---
+++ src/js/views/base.js
@@ -0,0 +1,26 @@
+export const elements = {
+  searchInput: document.querySelector('.search__field'),
+  searchForm: document.querySelector('.search'),
+  searchResList: document.querySelector('.results__list'),
+  searchRes: document.querySelector('.results'),
+};
+
+export const elementStrings = {
+  loader: 'loader',
+};
+
+export const renderLoader = (parentElement) => {
+  const loader = `
+		<div class="${elementStrings.loader}">
+			<svg>
+				<use href="img/icons.svg#icon-cw"></use>
+			</svg>
+		</div>
+	`;
+  parentElement.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const clearLoader = () => {
+  const loader = document.querySelector(`.${elementStrings.loader}`);
+  if (loader) loader.parentElement.removeChild(loader);
+};
