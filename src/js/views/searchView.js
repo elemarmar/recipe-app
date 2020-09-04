0b4f46ec6cca6ca53ea33d1e07914ecae647b469
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-09-04 15:47:43 +0200

    Add searchView

---
+++ src/js/views/searchView.js
@@ -0,0 +1,46 @@
+import { elements } from './base';
+
+// reading input from input form
+export const getInput = () => elements.searchInput.value;
+
+export const clearInput = () => {
+  elements.searchInput.value = '';
+};
+export const clearResults = () => {
+  elements.searchResList.innerHTML = '';
+};
+
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(' ').reduce((acc, current) => {
+      if (acc + current.length <= limit) {
+        newTitle.push(current);
+      }
+      return acc + current.length;
+    }, 0);
+    return `${newTitle.join(' ')} ...`;
+  }
+  return title;
+};
+
+const renderRecipe = (recipe) => {
+  const markup = `
+    <li>
+   		<a class="results__link" href="#${recipe.recipe_id}">
+    		<figure class="results__fig">
+    			<img src="${recipe.image_url}" alt="${recipe.title}">
+    		</figure>
+    		<div class="results__data">
+    			<h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+    			<p class="results__author">${recipe.publisher}</p>
+    		</div>
+    	</a>
+    </li>
+	`;
+  elements.searchResList.insertAdjacentHTML('beforeend', markup);
+};
+
+export const renderResults = (recipes) => {
+  recipes.forEach(renderRecipe);
+};
