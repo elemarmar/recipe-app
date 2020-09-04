29db06dcbe1a106d3ba7ab515eb9d1a178efaa26
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-09-04 15:48:37 +0200

    Render loader

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,38 @@
-// Global app controller
-console.log('hi');
+import Search from './models/Search';
+import { elements, renderLoader, clearLoader } from './views/base';
+import * as searchView from './views/searchView';
+
+/** Global state of the app
+ * - Search object -> all data about the search
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+  // 1. get query from the view
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2. create new Search object and add it to the state
+    state.search = new Search(query);
+
+    // 3. Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
+
+    // 4. Search for recipes
+    await state.search.getResults();
+
+    // 5. Render results on UI
+    clearLoader();
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+  controlSearch();
+});
