2fce1724d76fe696e6d3a2dff2964720c6bca3ae
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-09-05 11:18:36 +0200

    Add calcTime and calcServings methods

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -12,11 +12,23 @@ export default class Recipe {
       );
       this.title = res.data.recipe.title;
       this.author = res.data.recipe.publisher;
-      this.img = res.data.recipe.img_url;
+      this.img = res.data.recipe.image_url;
       this.url = res.data.recipe.source_url;
       this.ingredients = res.data.recipe.ingredients;
     } catch (error) {
       console.log(error);
+      alert('Something went wrong');
     }
+  }
+
+  // we assume that for every 3 ingredients, we need 15 min
+  calcTime() {
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
+
+  calcServings() {
+    this.servings = 4;
   }
 }
